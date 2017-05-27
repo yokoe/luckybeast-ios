21b1d8c8aca7958a412be02608e4af367e4fd300
Sota Yokoe <kreuz45@kreuz45.com>
2017-05-27 20:41:29 +0900

    indicator animation

--- luckybeast/BossCoreView.swift
+++ luckybeast/BossCoreView.swift
@@ -6,9 +6,15 @@ class BossCoreView: UIView {
     
     var mode: LuckyBeast.Mode = .idle {
         didSet {
+            if mode == .thinking {
+                startIndicatorTimer()
+            } else {
+                stopIndicatorTimer()
+            }
             setNeedsDisplay()
         }
     }
+    private var timerStartedAt: Date?
     
     override func draw(_ rect: CGRect) {
         UIColor.black.setFill()
@@ -42,16 +48,35 @@ class BossCoreView: UIView {
         let centerY = bounds.midY
         let indicatorRadius = min(centerX, centerY) - BossCoreView.indicatorPadding
         
-        UIColor.darkGray.setFill()
-        UIColor.darkGray.setStroke()
+        let timeElapsed = -(timerStartedAt?.timeIntervalSinceNow ?? 0)
+        let indicatorProgress = TimeInterval(Int(round(timeElapsed * 100)) % 100) * 0.01
         
         for i in 0...BossCoreView.indicatorDivisions {
-            let angle = CGFloat(i) / CGFloat(BossCoreView.indicatorDivisions) * 2 * CGFloat.pi
+            let alpha: CGFloat = indicatorProgress * 4 - TimeInterval(i) * 0.1 > 0 ? 1 : 0
+            UIColor(white: 1, alpha: min(alpha, 1)).setStroke()
+            
+            let angle = CGFloat(i) / CGFloat(BossCoreView.indicatorDivisions) * 2 * CGFloat.pi - CGFloat.pi * 0.5
             let path = UIBezierPath()
             path.move(to: CGPoint(x: centerX + cos(angle) * indicatorRadius, y: centerY + sin(angle) * indicatorRadius))
             path.addLine(to: CGPoint(x: centerX + cos(angle) * indicatorRadius * 0.5, y: centerY + sin(angle) * indicatorRadius * 0.5))
             path.lineWidth = 10
             path.stroke()
         }
+    }
+    
+    // Indicator timer
+    private var indicatorTimer: Timer?
+    private func startIndicatorTimer() {
+        if indicatorTimer == nil {
+            timerStartedAt = Date()
+            indicatorTimer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { timer in
+                self.setNeedsDisplay()
+            }
+        }
+    }
+    
+    private func stopIndicatorTimer() {
+        indicatorTimer?.invalidate()
+        indicatorTimer = nil
     }
 }
